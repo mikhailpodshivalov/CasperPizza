691cbeff67a1e60637b1824cc6da621e51007556
Mihail Podshivalov <mihailp@innstant.com>
2025-01-18 18:13:03 +0100

    Fixed errors, fixed modal windows

--- src/app/Jobs/SendOrderEmail.php
+++ src/app/Jobs/SendOrderEmail.php
@@ -2,26 +2,40 @@
 
 namespace App\Jobs;
 
+use App\Mail\OrderCreated;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 class SendOrderEmail implements ShouldQueue
 {
-    use Queueable;
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order;
+    protected $items;
+    protected $to;
 
     /**
      * Create a new job instance.
+     *
+     * @return void
      */
-    public function __construct()
+    public function __construct($order, $items, $to)
     {
-        //
+        $this->order = $order;
+        $this->items = $items;
+        $this->to    = $to;
     }
 
     /**
      * Execute the job.
+     *
+     * @return void
      */
-    public function handle(): void
+    public function handle()
     {
-        //
+        Mail::to($this->to)->send(new OrderCreated($this->order, $this->items));
     }
 }
